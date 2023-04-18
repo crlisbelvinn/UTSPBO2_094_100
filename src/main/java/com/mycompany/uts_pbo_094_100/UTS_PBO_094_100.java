e5feacaf17ba4ace801cf634eaafff776e7cb93a
unknown <xpopow1104@gmail.com>
2023-04-18 22:43:57 +0700

    Inisiasi projek

---
+++ src/main/java/com/mycompany/uts_pbo_094_100/UTS_PBO_094_100.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.uts_pbo_094_100;
+
+/**
+ *
+ * @author xpopow
+ */
+public class UTS_PBO_094_100 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
