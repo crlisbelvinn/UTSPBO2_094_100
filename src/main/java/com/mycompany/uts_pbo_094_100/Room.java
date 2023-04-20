fd587fdc72c6bde72baac10ea027a52cedf8a7e2
unknown <xpopow1104@gmail.com>
2023-04-21 02:08:26 +0700

    Menambah fitur menu

---
+++ src/main/java/com/mycompany/uts_pbo_094_100/Room.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.uts_pbo_094_100;
+
+/**
+ *
+ * @author xpopow
+ */
+public class Room {
+    String type;
+    double price;
+}
--- src/main/java/com/mycompany/uts_pbo_094_100/UTS_PBO_094_100.java
+++ src/main/java/com/mycompany/uts_pbo_094_100/UTS_PBO_094_100.java
@@ -4,13 +4,47 @@
 
 package com.mycompany.uts_pbo_094_100;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author xpopow
  */
 public class UTS_PBO_094_100 {
-
+    
+    static ArrayList<Room>daftarRoom;
+    
+    static void menu(){
+        System.out.println("Selamat datang di Hotel Jaya Jaya Jaya!!!");
+        System.out.println("-----------------------------------------");
+        System.out.println("1. Pilih Kamar");
+        System.out.println("2.");
+        System.out.println("3. Keluar");
+    }
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        daftarRoom = new ArrayList<>();
+        
+        Room r1 = new Room();
+        r1.type = "Economy";
+        r1.price = 250.0;
+        System.out.println(r1.type);
+        System.out.println(r1.price);
+        daftarRoom.add(r1);
+        
+        Room r2 = new Room();
+        r2.type = "Business";
+        r2.price = 700.0;
+        daftarRoom.add(r2);
+        
+        Room r3 = new Room();
+        r3.type = "Suite";
+        r3.price = 1300.0;
+        daftarRoom.add(r3);
+        
+        menu();
+        
+        System.out.println(daftarRoom.get(0));
     }
 }
